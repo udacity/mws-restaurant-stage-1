69778feb9ac9102329bdc21352b5e3d74fee094d
George <gfsgeo@gmail.com>
2018-02-23 19:11:55 +0200

    Audits on restaurant details page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -57,6 +57,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt=restaurant.name+` ${restaurant.cuisine_type} Restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
