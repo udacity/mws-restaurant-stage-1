6bc606ef1860fece40adff6153bc90b55dcf27e6
Adrian <adriandinca2008@yahoo.com>
2018-03-05 19:23:33 +0200

    chore: Add a polyfill for the `append` method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// append polyfill
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('append')) {
+      return;
+    }
+    Object.defineProperty(item, 'append', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function append() {
+        var argArr = Array.prototype.slice.call(arguments),
+          docFrag = document.createDocumentFragment();
+
+        argArr.forEach(function (argItem) {
+          var isNode = argItem instanceof Node;
+          docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+        });
+
+        this.appendChild(docFrag);
+      }
+    });
+  });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
 let restaurants,
   neighborhoods,
   cuisines
