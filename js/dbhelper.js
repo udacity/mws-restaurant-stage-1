1cf4f4106ed8c5500ecf38b38d464978a41d535b
George <gfsgeo@gmail.com>
2018-03-07 19:13:07 +0200

    feat : Utilized get restaurant by id, from db instead of getting all restaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -25,19 +25,10 @@ class DBHelper {
    * Fetch a restaurant by its ID.
    */
   static fetchRestaurantById(id, callback) {
-    // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
-      }
-    });
+   fetch(`${DBHelper.DATABASE_URL}${id}`).then(response => response.json())
+   .then(restaurant => callback(null,restaurant))
+   .catch(e => callback('Restaurant does not exist',null));
+
   }
 
   /**
