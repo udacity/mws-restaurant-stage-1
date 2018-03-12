b32e36f0a703ed07aac126f690e957fbd22b4ec4
Adrian <adriandinca2008@yahoo.com>
2018-03-12 17:36:48 +0200

    fix: Add proper aria labeling for the breadcrumb

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -171,6 +171,7 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.setAttribute('aria-current', 'page');
   breadcrumb.appendChild(li);
 }
 
