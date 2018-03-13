bf816c9a1849d2635a0b08e86b6076deea4987d2
Fidèle PALOUKI <fidele.palouki@gmail.com>
2018-03-13 13:46:25 +0100

    chore(gulp): add imagemin dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const imagemin = require('gulp-imagemin');
 
-gulp.task('images', function () {
+// TODO: Check why this is not working
+// gulp.task('images', gulp.series('compress', 'resize'));
+
+// gulp.task('compress', () => {
+//   return gulp.src('img_src/*.jpg')
+//       .pipe(imagemin({
+//           progressive: true
+//       }))
+//       .pipe(gulp.dest('img_src_temp'));       
+// });
+
+gulp.task('resize', () => {
   return gulp.src('img_src/*.{jpg,png}')
     .pipe($.responsive({
       // Convert all images to JPEG format
