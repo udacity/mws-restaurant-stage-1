77b2430831d1f85fdf060929698536fd83fb48ff
zwan <zhentian.wan@ssh.com>
2018-03-19 14:46:32 +0200

    service worker

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_VERSION = "restaurant_app_v5";
+
+self.addEventListener("install", event => {
+  const urlsToCache = [
+    "/",
+    "index.html",
+    "restaurant.html",
+    "js/main.js",
+    "js/restaurant_info.js",
+    "js/dbhelper.js",
+    "css/over640.css",
+    "css/over1024.css",
+    "css/styles.css",
+    "data/restaurants.json"
+  ];
+
+  event.waitUntil(
+    caches
+      .open(CACHE_VERSION)
+      .then(cache => {
+        console.log("cache all");
+        return cache.addAll(urlsToCache);
+      })
+      .catch(() => {
+        console.error("Cannot cache anything");
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    // Fetch the static assets
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    // Remove the old cache
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => {
+            return name.startsWith("restaurant") && name != CACHE_VERSION;
+          })
+          .map(name => {
+            return caches.delete(name);
+          })
+      );
+    })
+  );
+});
