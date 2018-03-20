bdd47e42d41e0165ecc2729a0554c1ada6219654
Michael Dittmer <mditterm-reg@yahoo.de>
2018-03-20 15:53:28 +0100

    Grid added

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -7,15 +7,17 @@ var map;
 window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
-      console.error(error);
+      //console.error('this error:' + error);
     } else {
       self.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
         center: restaurant.latlng,
         scrollwheel: false
       });
+      
       fillBreadcrumb();
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      
     }
   });
 }
