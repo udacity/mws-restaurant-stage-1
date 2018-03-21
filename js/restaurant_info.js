31dce32f1dc31ffb312ecde60837a86ea505db2b
Fidèle PALOUKI <fidele.palouki@gmail.com>
2018-03-21 23:51:06 +0100

    fix(Ay11): forgot to add alt text to restaurant img !

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = DBHelper.imageAltForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
