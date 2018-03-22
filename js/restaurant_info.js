d0472cfb0f180c969d7612351c87ef4ffd924156
Zhentian Wan <answer881215@gmail.com>
2018-03-22 15:43:00 +0200

    add sw for resturant info page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,5 +1,21 @@
+(() => {
+    window.addEventListener('load', () => {
+        if ('serviceWorker' in navigator) {
+            // Register a service worker hosted at the root of the
+            // site using the default scope.
+            navigator.serviceWorker.register('/sw.js').then(function (registration) {
+                console.log('Service worker registration succeeded:', registration);
+            }).catch(function (error) {
+                console.log('Service worker registration failed:', error);
+            });
+        } else {
+            console.log('Service workers are not supported.');
+        }
+    });
+})();
+
 let restaurant;
-var map;
+let map;
 
 /**
  * Initialize Google map, called from HTML.
