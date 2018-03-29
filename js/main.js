f8b06a067f2ac7db10048db6859d85a08183eb9f
fcarmona <fcarmona.olmedo@gmail.com>
2018-03-30 00:06:28 +0200

    wip: Restaurants list

--- js/main.js
+++ js/main.js
@@ -137,6 +137,8 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.className = 'restaurant-info';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
@@ -145,23 +147,25 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  div.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  div.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  div.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  div.append(more);
+
+  li.append(div);
 
   return li
-}
+};
 
 /**
  * Add markers for current restaurants to the map.
