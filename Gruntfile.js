d87d4c235dbe40cfb0d22cd3c13ec939e54f91e2
tawfiek <tawfiek.108@gmail.com>
2018-03-29 22:24:08 +0200

    syle : make Home page Resposive adding style to home page nav Title to looks nicestyle style Filter Result par and make it responsive using column drop Resposive pattern with restaurant list get image sizes ready for resposive

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+           name: 'small',
+           width: '460px',
+           suffix: '_1x',
+           quality: 25
+          },{
+            name: 'larg',
+            width: '100%',
+            suffix: '_2x',
+            quality: 50
+          }]
+        },
+
+    
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
