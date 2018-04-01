829eef3564ddba1a8ec08e5d84b0894d27aa2da5
tawfiek <tawfiek.108@gmail.com>
2018-04-01 21:36:33 +0200

    feat: make app work offline first

--- js/main.js
+++ js/main.js
@@ -3,6 +3,24 @@ let restaurants,
   cuisines
 var map
 var markers = []
+ 
+
+/**
+ * Register Service Worker
+ */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
@@ -180,3 +198,5 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+
