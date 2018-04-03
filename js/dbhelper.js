5d8325f67ff9dd9fd8f4beaa5b2e05e4c83a35cc
tawfiek <tawfiek.108@gmail.com>
2018-04-03 05:34:01 +0200

    fix: register service worker in dbhelper & put meta tag on the top

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,6 +1,21 @@
 /**
  * Common database helper functions.
  */
+/**
+ * Register Service Worker
+ */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 class DBHelper {
 
   /**
