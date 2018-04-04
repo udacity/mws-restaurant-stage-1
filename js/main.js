30f17e534b338dc8a86a018ff1a2816d9fb5dbfa
tawfiek <tawfiek.108@gmail.com>
2018-04-04 18:06:38 +0200

    fix: let restaurant name in h2 in home page -adding lang artt in html tag in poth pages

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('tabindex', '0');
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
