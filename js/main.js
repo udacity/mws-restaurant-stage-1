b8898a7ffcec6bda5a9ed18c57e8c39a4a395dad
Marcamillian <marcamillian@gmail.com>
2018-04-04 12:45:19 +0100

    feat: Register service worker & cache some resources
    The service worker is registering but it is not pulling the cached resources
    on fetch requests when offline. Need to fix this

--- js/main.js
+++ js/main.js
@@ -4,6 +4,65 @@ let restaurants,
 var map
 var markers = []
 
+let updateDialog = document.querySelector('.update-dialog');
+let updateInstallButton = document.querySelector('.update-dialog .install-udpate');
+
+const serviceWorkerHelper = function ServiceWorkerHelper(workerLocation, updateUI, updateTriggerEl){
+  if (!navigator.serviceWorker) throw new Error("service worker not supported")
+
+  const updateTriggerElement = updateTriggerEl;
+
+  // register the service worker
+  navigator.serviceWorker.register(workerLocation).then((reg)=>{
+      
+      // check if service worker loaded the page - if it didn't return (as service worker is the latest)
+      if (!navigator.serviceWorker.controller) return
+      
+      // if there is one waiting - there was a service worker installed on the last refresh and its waiting
+      if(reg.waiting){
+          displayHelper.revealPopup(updateUI)
+          return;
+      }
+
+      // if there is a service worker installing
+      if(reg.installing){
+          trackInstalling(reg.installing)
+          return;
+      }
+
+      // listen for future workers installing
+      reg.addEventListener('updatefound', ()=>{
+          trackInstalling(reg.installing)
+      })
+
+
+  }).catch((err)=>{
+      throw new Error(`Service worker didn't register: ${err.message}`)
+  })
+
+  // listen for changeover of service worker - reload page if a new one took over
+  navigator.serviceWorker.addEventListener('controllerchange', ()=>{
+      window.location.reload()
+  })
+
+
+  // listen to installing service worker && show user when its waiting
+  const trackInstalling = (worker)=>{
+
+      worker.addEventListener('statechange', ()=>{
+          if(worker.state == 'installed'){
+
+              updateTriggerElement.addEventListener('click', ()=>{ // add click event to the UI
+                  worker.postMessage({action: 'skipWaiting'})
+              })
+
+              displayHelper.revealPopup(updateUI)  // show the UI
+          }
+      })
+  }
+
+}('./js/sw.js', updateDialog, updateInstallButton)
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
---
+++ js/sw.js
@@ -0,0 +1,58 @@
+//
+
+const staticCacheName = "restaurant-static-v1"
+
+self.addEventListener('install',(event)=>{  // do things when the service worker installs
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache)=>{
+            return cache.addAll([
+                '/',
+                '/restaurant.html',
+                '/js/main.js',
+                '/js/dbhelper.js',
+                '/js/restaurant_info.js',
+                '/css/styles.css',
+                '/css/over450.css',
+                '/data/restaurants.json'
+            ])
+        })
+    )
+})
+
+
+self.addEventListener('activate', (event)=>{   // when active -delete the outdated caches
+    // delte the caches that arn't the current one
+    event.waitUntil(
+        caches.keys().then((cacheNames)=>{
+            return Promise.all(
+                cacheNames.filter((cacheName)=>{
+                    return cacheName.startsWith('restaurant') &&
+                            cacheName != staticCacheName
+                }).map((cacheName)=>{
+                    return caches.delete(cacheName)
+                })
+            )
+        })
+    )
+})
+
+// TODO: The resources are cached but not being fetched from cache when offline
+self.addEventListener('fetch', (event)=>{   // listening for calls to fetch
+    const requestUrl = new URL(event.request.url)
+    
+    // == get things from cache first || get from network if not in the cache
+    event.respondWith(
+        caches.match(event.request).then((response)=>{  // look in the caches for the response
+            return response || fetch(event.request) // if no response - get it from the network
+        })
+    )
+
+})
+
+self.addEventListener('message', (event)=>{ // listening to messages to service worker
+
+    // if a message has been sent to have the installed service worker stop waiting
+    if(event.data.action == 'skipWaiting'){
+        self.skipWaiting()  // tell the service worker to install itself
+    }
+})
