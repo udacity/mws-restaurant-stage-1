341236311ee2e60714cfbbe7ccbd85c503f2b1b9
Marcamillian <marcamillian@gmail.com>
2018-04-05 17:33:12 +0100

    feat: Add srcset attribute to the restaurant.html images
    Added the srcet attribute so that the restautant pictures are shown
    correctly on the reviews page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -55,10 +55,15 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
 
-    // TODO: correct the image element to be correct for the new format
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+
+  const baseImageUrl = DBHelper.imageUrlForRestaurant(restaurant);
+  const urlComponents = baseImageUrl.split(".");
+  // TODO: Increase the quality of the pictures for bigger sizes
+  image.src = `${urlComponents[0]}-400_1x.${urlComponents[1]}`; // src for fallback
+  image.srcset = `${urlComponents[0]}-400_1x.${urlComponents[1]} 1x,
+                  ${urlComponents[0]}-800_2x.${urlComponents[1]} 2x`
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
