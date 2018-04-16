af4b4280cb745b5ae248a2d2699edf0686ac7f96
Ichi <ichibunny@hotmail.com>
2018-04-17 00:17:01 +0100

    feat: Updated SW

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -57,7 +57,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
-  image.setAttribute('alt', restaurant.name );
+  image.setAttribute('alt', restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
--- sw.js
+++ sw.js
@@ -1,52 +1,73 @@
 
-const version= '1';
-const staticCacheName = "review-v" + version;
+const version= '7';
+const reviewCacheName = "restaurant-v" + version;
+const dynamicCacheName = "d-restaurant-v" + version;
 const staticFiles = [
+    "/",
     "index.html",
     "restaurant.html",
     "css/styles.css",
     "data/restaurants.json",
     "js/dbhelper.js",
-    "js/restaurant_info.js"
+    "js/restaurant_info.js",    
 ];
 
 // install
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(staticCacheName).then((cache) => {
+        caches.open(reviewCacheName).then((cache) => {
             return cache.addAll(staticFiles);
         })
     );
 });
 
+ 
 // activate 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then ((cacheNames)=>{
-      return Promise.all (
-        cacheNames.filter().filter(function(cacheName){
-        return cacheName != staticCacheName;
-        }).map(function (cacheName){
-          return caches.delete(cacheName);
+    event.waitUntil(
+      caches.keys().then ((cacheNames)=>{
+        return Promise.all (
+          cacheNames.filter((cacheName)=>{
+          return cacheName != reviewCacheName;
+          }).map(function (cacheName){
+            return caches.delete(cacheName);
+          })
+        )  
+      })
+    );
+  });
+ 
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(dynamicCacheName)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
         })
-      )  
-    })
-  );
-});
-fetch
-self.addEventListener('fetch', (event)=>{
-    event.respondWith(    
-        caches.match(event.request).then(response=>{
-            // return the orininal request to network
-            return response || fetch(event.request);
-            })
-        );
-    });
-
-// message
-self.addEventListener ('message', function (event){
-
-    if(event.data.action == 'skipwaiting'){
-        self.skipWaiting();
-    }
-});
+      );
+  });
