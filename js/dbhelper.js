e9d801c156b2b4b492dc9cb9b2afa4ac87747c41
Sean McBride <spmcbride1201@gmail.com>
2018-05-07 14:08:09 -0400

    chore: fix port

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,7 +7,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 3000; // Change this to your server port
+    const port = 8080; // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
