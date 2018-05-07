06ed9544adea12747dd1e66c8f2c9eee4762d025
Marcamillian <marcamillian@gmail.com>
2018-05-07 18:14:14 +0100

    feat: Add idb call to the restaurant.html page
    Changed the onload to use the promisified call to dbHelper rather than
    calling from the network

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,23 +1,28 @@
 let restaurant;
 var map;
+let dbHelper = new DBHelper()
 
 /**
  * Initialize Google map, called from HTML.
  */
 window.initMap = () => {
-  fetchRestaurantFromURL((error, restaurant) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
-      });
-      fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
-    }
-  });
+
+  dbHelper.getRestaurantById(getParameterByName('id'))
+  .then((restaurant)=>{ // set the restaurant details
+    self.restaurant = restaurant;
+    fillRestaurantHTML(restaurant)
+    return restaurant;
+  })
+  .then((restaurant)=>{  // set the map details
+    self.map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 16,
+      center: self.restaurant.latlng,
+      scrollwheel: false
+    })
+    self.fillBreadcrumb();
+    DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+  })
+
 }
 
 /**
