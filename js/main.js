f7575b9b069da616c324a0fe9ab0f81bbf8ccdbb
benreckas <benreckas@gmail.com>
2018-05-22 16:57:28 -0700

    feat(styles) fixing styles for restuarant list

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let restaurants,
-  neighborhoods,
-  cuisines
+    neighborhoods,
+    cuisines
 var map
 var markers = []
 
@@ -140,25 +140,31 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const info = document.createElement('div');
+  info.className = 'restaurant-info'
+  li.append(info);
+
   const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  info.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  info.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  info.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('aria-label', restaurant.name);
+  info.append(more)
 
   return li
 }
@@ -176,3 +182,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
+
+/**
+ * Skip links logic
+ */
+const skipLink = document.getElementById('skip-link');
+
+skipLink.addEventListener('click', (e) => {
+  document.getElementById('filter-header').focus();
+});
