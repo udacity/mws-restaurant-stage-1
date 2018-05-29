3c6827664774faafa7129b3e32157f5f1d73a6b1
Brian Freeman <atlanta_dancer@hotmail.com>
2018-05-28 20:00:58 -0400

    Initial responsive and accessibility updates

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  // add accessibility alt text identify restaurant shown 
+  image.alt = restaurant.name;
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
