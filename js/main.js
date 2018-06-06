463bd58533279aede42b80a6af9971db5807e2cf
agonzalez0515 <agonzalez0515@gmail.com>
2018-06-05 22:40:05 -0700

    Refactor: Resize images, add alt tag, and add language info to html

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = " "
   li.append(image);
 
   const name = document.createElement('h1');
