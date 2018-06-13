8b0369a5f44ccfa08c1812d7ee8ba9a74b5287a2
Omer Tarik <omertarik96@gmail.com>
2018-06-13 16:00:56 -0500

    images alt tags added

--- js/main.js
+++ js/main.js
@@ -159,6 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " restaurant marketing image";
   li.append(image);
 
   const name = document.createElement('h1');
