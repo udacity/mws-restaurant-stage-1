f086a4d4fa84d2c29f7f6b07f6a584c671373976
thanapoom21 <thanapoom.p.music@gmail.com>
2018-06-13 19:18:50 -0700

    modified:   restaurant.html
    fix: add attributes
    
    insert aria-label for a screen reader and set role of a map to an application.
    fix semantic tags h1, h2, h3`

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", DBHelper.imageAltForRestaurant(restaurant));
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
