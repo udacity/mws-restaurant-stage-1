9a36e535ab8a39f4de6a31301380448b565caa75
Ron <gr8tcode@gmail.com>
2018-06-17 19:24:47 -0400

    remove indexcontroller.js

--- indexcontroller.js
+++
@@ -1,7 +0,0 @@
-IndexCOntroller.prototype._registerServiceWorker = function() {
-    if ( !navigator.serviceWorker ) return;
-
-    navigation.serviceWorker.register( '/sw.js' ).then( function() {
-        console.log( )
-    })
-}
