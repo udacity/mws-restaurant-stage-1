e9ef212cb3bed74831fb649b9cb7eb516e1fd368
Ron <gr8tcode@gmail.com>
2018-06-17 01:47:40 -0400

    service worker

--- indexcontroller.js
+++ indexcontroller.js
@@ -1,45 +1,7 @@
-IndexController.prototype._registerServiceWorker = function() {
-    if (!navigator.serviceWorker) return;
+IndexCOntroller.prototype._registerServiceWorker = function() {
+    if ( !navigator.serviceWorker ) return;
 
-    var indexController = this;
-
-    navigator.serviceWorker.register('/sw.js').then(function(register) { 
-        if (!navigator.serviceWorker.controller) { return; }
-
-        if ( register.waiting ) {
-            indexController._updateReady();
-            return;
-        }
-
-        if ( register.installing ) {
-            indexController._trackInstalling( register.installing );
-            return;
-        }
-
-        register.addEventListener( 'updatefound', function() {
-            indexController._trackInstalling( register.installing );
-            return;
-        });
-    });
-};
-
-IndexController.prototype._trackInstalling = function(worker) {
-    var indexController = this;
-
-    worker.addEventListener( 'statechange', function() {
-        if ( worker.state === 'installed' ) {
-            indexController._updateReady();
-        }
-    });
-};
-
-IndexController.prototype._updateReady = function(worker) {
-    var toast = this._toastsView.show("New version available", {
-        buttons: [ 'refresh', 'dismiss']
-    });
-
-    toast.answer.then(function(answer) {
-        if (answer !== 'refresh') return;
-        worker.postMessage({ action: 'skipWaiting' });
-    });
-};
+    navigation.serviceWorker.register( '/sw.js' ).then( function() {
+        console.log( )
+    })
+}
--- sw.js
+++ sw.js
@@ -1,43 +1,51 @@
-/*self.addEventListener( 'fetch', function(event) {
-    console.log(event.request);
-});*/
-var staticCacheName = 'Restaurant-Reviews-v2';
+/*var log = console.log.bind(console);
+var version = "0.0.2";
+var cacheName = "sw";
+var cache = cacheName + "-" + version;
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/img',
+    '/css/styles.css',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurant.json'
+];*/
 
-self.addEventListener( 'install', function(event) {
-    event.waitUntil (
-        caches.open(staticCacheName).then( function(cache) {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'restaurant.html',
-                'css/styles.css'
-            ]);
-        })
-    );
+var CACHE = 'cache-only';
+
+ 
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+
+
+ 
+  evt.waitUntil(precache());
 });
 
-/*self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match( event.request ).then( function( response ) {
-            return response || fetch(event.request);
-        }).catch( function( error ) {
-            console.log( error, 'no cache entry for:', event.request.url );
-        })
-    );
-});*/
 
-self.addEventListener('activate', function(event){
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('Restaurant-Reviews-') && cacheName !== staticCacheName
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-})
+ 
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+});
+
+ 
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './controlled.html',
+      './asset'
+    ]);
+  });
+}
+
+ 
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
