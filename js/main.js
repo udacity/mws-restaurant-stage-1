e1eeefd71d2b6ad5f942c4df3ab9a89a065a9504
scrabill <shannon.crabill@gmail.com>
2018-06-20 22:08:59 -0400

    Adds <h2> to resturant name in main.js

--- js/main.js
+++ js/main.js
@@ -148,7 +148,7 @@ createRestaurantHTML = (restaurant) => {
   /*const alt = document.createElement('alt'); */
 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
