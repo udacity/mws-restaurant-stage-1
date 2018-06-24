053451fece8145e07dc0e0175abe1743a504249a
Denise R James <denisetoo@gmail.com>
2018-06-24 10:12:17 -0500

    Add files via upload

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,18 +1,6 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('' +
-        'load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
 
 let restaurant;
-var newMap;
+var map;
 
 /**
  * Initialize map as soon as the page is loaded.
