1c1e9cc0dec91a97274eb17420ba0d15ded7fc7b
Laurie Linz <laura.linz@ca.com>
2018-06-25 11:56:57 -0600

    Add alt tag to javascript

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name + " restaurant promotional image";
   li.append(image);
 
   const name = document.createElement('h1');
