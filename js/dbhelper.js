216515ca9680571da220e956c9e7efb1129b469a
FERREIRA DE OLIVEIRA Rafael <rafael.ferreira-de-oliveira@ext.ec.europa.eu>
2018-06-28 14:14:48 +0200

    using REST api instead of static json to fetch the restaurants

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,10 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    //const port = 8000; // Change this to your server port
+    const port = 1337; // Change this to your server port
+    //return `http://localhost:${port}/data/restaurants.json`;
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
@@ -21,7 +23,8 @@ class DBHelper {
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        //const restaurants = json.restaurants;
+        const restaurants = json;
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
@@ -150,14 +153,14 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
     /**
    * Restaurant thumbnail image URL.
    */
   static thumbnailImageUrlForRestaurant(restaurant) {
-    return (`/img/thumbnails/${restaurant.photograph}`);
+    return (`/img/thumbnails/${restaurant.photograph}.jpg`);
   }
 
   /**
