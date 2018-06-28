4298f0a295a2a5a02d924f1ab241f4affe15e39f
FERREIRA DE OLIVEIRA Rafael <rafael.ferreira-de-oliveira@ext.ec.europa.eu>
2018-06-28 18:05:50 +0200

    setting up concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
 gulp.task('default', defaultTask);
+gulp.task('scripts', scripts);
 
 function defaultTask(done) {
-
-	gulp.watch('index.html', ['trackChanges']);
-
 	done();
 }
 
-gulp.task('trackChanges', trackChanges);
+function scripts() {
+	gulp.src('js/**/*.js')
+		.pipe(gulp.dest('dist/js'));
+}
 
-function trackChanges() {
-	console.log('index changed');
+function scriptsDist() {
+	console.log('todo');
 }
