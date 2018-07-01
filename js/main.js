bae40b0619f5ce3dceaae432fd994837e5f1a16f
Olga Vorozheykina <solariskind@gmail.com>
2018-07-01 14:57:07 +0300

    feat: Added alt text for restaurant links

--- js/main.js
+++ js/main.js
@@ -173,8 +173,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  const restaurantId = DBHelper.idForRestaurant(restaurant);
-  image.alt = altText[restaurantId];
+  image.alt = altText[restaurant.id];
   li.append(image);
 
   const name = document.createElement('h1');
@@ -192,7 +191,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute('aria-label', restaurant.name + '. View details');
+  li.append(more);
 
   return li
 }
