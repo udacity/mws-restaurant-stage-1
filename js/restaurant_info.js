6b11ead02287d4af8d418f93678a06e2ea494f1a
Piotr Ładoński <piolado@gmail.com>
2018-07-03 21:12:14 +0200

    feat: Add initial working mobile version
    Most elements were moved to the flexbox
    Contrast ratio of all text was adjusted to be over 4.5
    Currently it is mobile only (i.e looks weird on desktop)

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -149,14 +149,17 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.classList.add('review-name');
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.classList.add('review-date');
   date.innerHTML = review.date;
   li.appendChild(date);
 
   const rating = document.createElement('p');
+  rating.classList.add('review-rating');
   rating.innerHTML = `Rating: ${review.rating}`;
   li.appendChild(rating);
 
