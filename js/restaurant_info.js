70488f72539e01d485f1e8e546a68d6e42ddad50
khusbuchandra <khushboo.chandra@gmail.com>
2018-07-03 22:28:46 +0530

    Added responsiveness to Restaurant reviews

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia2h1c2J1Y2hhbmRyYSIsImEiOiJjamozMm9oeGUwdGlkM3BwNjBwcndoY3NsIn0.nBnmq4CLCWPlszD2pl5Yvg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
