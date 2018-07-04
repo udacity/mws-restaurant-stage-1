ddf49221e0c77f1f55103e26a05fb29234c98c63
Mohamed Sayed <mohamedsayed008@gmail.com>
2018-07-04 12:04:58 +0200

    feat: Added Lazy-loading to main page images

--- app/js/main.js
+++ app/js/main.js
@@ -153,15 +153,129 @@ const fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 const createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-console.log('here',restaurant);
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('data-src', DBHelper.imageUrlForRestaurant(restaurant));
   image.srcset = DBHelper.imageSrcsetForRestaurant(restaurant);
   image.sizes = '(max-width: 667px) 81vw, (max-width: 899px) 33vw, 1vw';
   image.alt = `${restaurant.name} ${restaurant.cuisine_type} Restaurant`;
   image.tabIndex = 0;
   li.append(image);
+
+  // Get all of the images that are marked up to lazy load
+const images = document.querySelectorAll('.restaurant-img');
+const config = {
+  // If the image gets within 50px in the Y axis, start the download.
+  rootMargin: '50px 0px',
+  threshold: 0.01
+};
+
+let imageCount = images.length;
+let observer;
+
+// If we don't have support for intersection observer, loads the images immediately
+if (!('IntersectionObserver' in window)) {
+  loadImagesImmediately(images);
+} else {
+  // It is supported, load the images
+  observer = new IntersectionObserver(onIntersection, config);
+
+  // foreach() is not supported in IE
+  for (let i = 0; i < images.length; i++) { 
+    let image = images[i];
+    if (image.classList.contains('restaurant-img--handled')) {
+      continue;
+    }
+
+    observer.observe(image);
+  }
+}
+
+/**
+ * Fetchs the image for the given URL
+ * @param {string} url 
+ */
+function fetchImage(url) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.onload = resolve;
+    image.onerror = reject;
+  });
+}
+
+/**
+ * Preloads the image
+ * @param {object} image 
+ */
+function preloadImage(image) {
+  const src = image.dataset.src;
+  if (!src) {
+    return;
+  }
+
+  return fetchImage(src).then(() => { applyImage(image, src); });
+}
+
+/**
+ * Load all of the images immediately
+ * @param {NodeListOf<Element>} images 
+ */
+function loadImagesImmediately(images) {
+  // foreach() is not supported in IE
+  for (let i = 0; i < images.length; i++) { 
+    let image = images[i];
+    preloadImage(image);
+  }
+}
+
+/**
+ * Disconnect the observer
+ */
+function disconnect() {
+  if (!observer) {
+    return;
+  }
+
+  observer.disconnect();
+}
+
+/**
+ * On intersection
+ * @param {array} entries 
+ */
+function onIntersection(entries) {
+  // Disconnect if we've already loaded all of the images
+  if (imageCount === 0) {
+    observer.disconnect();
+  }
+
+  // Loop through the entries
+  for (let i = 0; i < entries.length; i++) { 
+    let entry = entries[i];
+    // Are we in viewport?
+    if (entry.intersectionRatio > 0) {
+      imageCount--;
+
+      // Stop watching and load the image
+      observer.unobserve(entry.target);
+      preloadImage(entry.target);
+    }
+  }
+}
+
+/**
+ * Apply the image
+ * @param {object} img 
+ * @param {string} src 
+ */
+function applyImage(img, src) {
+  // Prevent this from being lazy loaded a second time.
+  img.classList.add('restaurant-img--handled');
+  img.src = src;
+  img.classList.add('fade-in');
+}
 
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
@@ -171,7 +285,7 @@ console.log('here',restaurant);
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
   neighborhood.tabIndex = 0;
-  neighborhood.setAttribute('aria-label', `neighborhood town : ${restaurant.neighborhood}`)
+  neighborhood.setAttribute('aria-label', `neighborhood town : ${restaurant.neighborhood}`);
   li.append(neighborhood);
 
   const address = document.createElement('p');
