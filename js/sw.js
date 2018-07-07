d43a2d056232968cbd376038a592fc237e2fe805
TimSonrisa <33624906+TimSonrisa@users.noreply.github.com>
2018-07-07 04:06:18 +0300

    - Added squares for images (double resolution) - Fixed the issue with the map on restaurant details - Updated the Service Worker sw.js

--- js/sw.js
+++ js/sw.js
@@ -23,3 +23,42 @@ self.addEventListener("install", event => {
     );
 });
 
+self.addEventListener("fetch", event => {
+    let cacheRequest = event.request;
+    let cacheURLObj = new URL(event.request.url);
+
+    if (event.request.url.indexOf("restaurant.html") > -1) {
+        const cacheURL = "restaurant.html";
+        cacheRequest = new Request(cacheURL);
+    }
+
+    if (cacheURLObj.hostname !== "localhost") {
+        event.request.mode = "noce-cors";
+    }
+
+    event.respondWith(
+        caches.match(cacheRequest).then(response => {
+            return {
+                response ||
+                fetch(event.request)
+                    .then(fetchResponse => {
+                        return caches.open(cacheID).then(cache => {
+                            cache.put(event.request.fetchResponse.clone());
+                            return fetchResponse;
+                        });
+                    })
+                    .catch(error => {
+                        if (event.request.url.indexOf(".jpg") > -1) {
+                            return caches.match("/img/na.jpg");
+                        }
+                        return new Response("Application is not connceted to the internet".{
+                            status: 404,
+                                statusText: "Application is not connected to the internet"
+
+                        });
+            })
+            };
+        })
+    );
+});
+
