ea9932d3713595a9f02cc64abb2935904186ef94
Piotr Ładoński <piolado@gmail.com>
2018-07-08 02:58:37 +0200

    refactor: change Promise.then to async/await

--- sw.js
+++ sw.js
@@ -1,45 +1,37 @@
-let staticCacheName = 'mws-pro1-static-v2';
+let staticCacheName = 'mws-pro1-static-v3';
 let imgCacheName = 'mws-pro1-images';
 let allCaches = [
     staticCacheName,
     imgCacheName
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/restaurant.html',
-                '/css/styles.css',
-                '/js/',
-                '/js/private.js',
-                '/js/idb.js',
-                '/js/idbhelper.js',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/js/register.js',
-                '/js/lazysizes.min.js'
-            ]);
-        })
-    );
+self.addEventListener('install', async event => {
+    event.waitUntil((await caches.open(staticCacheName)).addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/css/styles.css',
+        '/js/',
+        '/js/private.js',
+        '/js/idb.js',
+        '/js/idbhelper.js',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/js/register.js',
+        '/js/lazysizes.min.js'
+    ]));
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cacheName => {
-                    return cacheName.startsWith('mws-pro1-') &&
-                        !allCaches.includes(cacheName);
-                }).map(cacheName => {
-                    return caches.delete(cacheName);
-                })
-            );
+self.addEventListener('activate', async event => {
+    event.waitUntil(Promise.all(
+        (await caches.keys()).filter(cacheName => {
+            return cacheName.startsWith('mws-pro1-') &&
+                !allCaches.includes(cacheName);
+        }).map(cacheName => {
+            return caches.delete(cacheName);
         })
-    );
+    ));
 });
 
 self.addEventListener('fetch', event => {
@@ -60,28 +52,20 @@ self.addEventListener('fetch', event => {
     }
 });
 
-function serveOther(cacheRequest) {
-    return caches.match(cacheRequest).then(response => {
-        return response || fetch(cacheRequest);
-    });
+async function serveOther(cacheRequest) {
+    return (await caches.match(cacheRequest)) || fetch(cacheRequest);
 }
 
-function serveRestaurant(cacheRequest) {
-    return caches.match(cacheRequest.url.split('?')[0]).then(response => {
-        return response || fetch(cacheRequest);
-    });
+async function serveRestaurant(cacheRequest) {
+    return (await caches.match(cacheRequest.url.split('?')[0])) || fetch(cacheRequest);
 }
 
-function servePhoto(request) {
+async function servePhoto(request) {
     let storageUrl = (new URL(request.url)).pathname.replace(/_\dx\.jpg$/, '');
-    return caches.open(imgCacheName).then(function (cache) {
-        return cache.match(storageUrl).then(function (response) {
-            if (response) return response;
-
-            return fetch(request).then(function (networkResponse) {
-                cache.put(storageUrl, networkResponse.clone());
-                return networkResponse;
-            });
-        });
-    });
+    let cache = await caches.open(imgCacheName);
+    let response = await cache.match(storageUrl);
+    if (response) return response;
+    let networkResponse = await fetch(request);
+    cache.put(storageUrl, networkResponse.clone());
+    return networkResponse;
 }
