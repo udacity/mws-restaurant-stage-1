4e55fe94a340d14b6f176c0cb3ff40f2a265e941
Alberto Manfrinati <alberto.manfrinati@gmail.com>
2018-07-08 18:53:42 +0200

    feat: Add gruntfile
    Add gruntfile to implement images resize.
    Also add package.json and .gitignore.

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          concurrency: 4,
+          engine: 'gm',
+          sizes: [{
+            width: 500,
+            suffix: '_medium',
+            quality: 50
+          }, {
+            width: 270,
+            suffix: '_thumbnail',
+            quality: 40
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.jpg'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    copy: {
+      main: {
+        expand: true,
+        flatten: true,
+        src: 'img/*',
+        dest: 'images/',
+      },
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images', 'copy']);
+
+};
