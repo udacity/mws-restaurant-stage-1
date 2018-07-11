c4f36b35678274638a97324dcf0968890ad93de5
FERREIRA DE OLIVEIRA Rafael <rafael.ferreira-de-oliveira@ext.ec.europa.eu>
2018-07-11 15:58:25 +0200

    improving the cache

--- sw.js
+++ sw.js
@@ -2,16 +2,17 @@
  * Service Worker configurations
  */
 
-const APP_VERSION = 'v3';
+const APP_VERSION = 'v2';
 var staticCacheName = `mws-static-${APP_VERSION}`;
 var mapCacheName = `mws-map-${APP_VERSION}`;
+var fontsCacheName = `mws-map-fonts-${APP_VERSION}`;
 
 const GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCpOS0fLi4JISp3kekVBcJGvzkXvgM1KZw&libraries=places&callback=initMap';
 const GOOGLE_MAPS_COMMON_URL = 'https://maps.googleapis.com/maps-api-v3/api/js/33/6a/common.js';
 const GOOGLE_MAPS_UTIL_URL = 'https://maps.googleapis.com/maps-api-v3/api/js/33/6a/util.js';
 const GOOGLE_MAPS_MAP_URL = 'https://maps.googleapis.com/maps-api-v3/api/js/33/6a/map.js';
 
-var allCaches = [staticCacheName, mapCacheName];
+var allCaches = [staticCacheName, mapCacheName, fontsCacheName];
 
 
   self.addEventListener('install', function(event) {
@@ -20,7 +21,7 @@ var allCaches = [staticCacheName, mapCacheName];
 
         return cache.addAll([
           'sw.js',
-          '/index.html',
+          'index.html',
           'restaurant.html',
           'dist/js/dbhelper.js',
           'dist/js/idb.js',
@@ -29,22 +30,7 @@ var allCaches = [staticCacheName, mapCacheName];
           'dist/js/vendor/idb.js',
           'css/breakpoints.css',
           'css/styles.css'
-        ])/* .then(function(){
-          return caches.open('OpaqueCache').then(function(opaqueCache) {
-            fetch(GOOGLE_MAPS_URL, {mode:'no-cors'}).then(function(response) {
-                return opaqueCache.put(GOOGLE_MAPS_URL, response);
-            });
-            fetch(GOOGLE_MAPS_COMMON_URL, {mode:'no-cors'}).then(function(response) {
-              return opaqueCache.put(GOOGLE_MAPS_COMMON_URL, response);
-            });
-            fetch(GOOGLE_MAPS_UTIL_URL, {mode:'no-cors'}).then(function(response) {
-              return opaqueCache.put(GOOGLE_MAPS_UTIL_URL, response);
-            });
-            fetch(GOOGLE_MAPS_MAP_URL, {mode:'no-cors'}).then(function(response) {
-              return opaqueCache.put(GOOGLE_MAPS_MAP_URL, response);
-            });            
-          });     
-        })*/
+        ])
       })
     );
   });
@@ -66,38 +52,46 @@ var allCaches = [staticCacheName, mapCacheName];
 
 self.addEventListener('fetch', function(event) {
     var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin || requestUrl.host === 'fonts.gstatic.com') {
+
+    //Always serve local content from cache
+    if (requestUrl.origin === location.origin) {
       if (requestUrl.pathname === '/') {
         event.respondWith(caches.match('index.html'));
         return;
       }
-      //TODO Check if this is really needed
       if (requestUrl.pathname.includes('restaurant.html')) {
         event.respondWith(caches.match('restaurant.html'));
         return;
-      } 
+      }      
       event.respondWith(
         caches.match(requestUrl).then(function(response) {
           return response || fetch(requestUrl);
         })
       );
     } else {
+      let requestHeader = {};
       if (requestUrl.host === 'maps.googleapis.com' || requestUrl.host === 'maps.gstatic.com') {
-        event.respondWith(
-          caches.match(requestUrl).then(function(response) {
-            if (response) return response;
-            return caches.open(mapCacheName).then(function(opaqueCache) {
-              return fetch(requestUrl, {mode:'no-cors'}).then(function(response) {
-                let clonedResponse = response.clone();
-                opaqueCache.put(requestUrl, clonedResponse);
-                return response;
-              });
-          })
-        }).catch(function(response) {
-          console.log('no cache', response);
-        }));
-        return;   
+        requestHeader.mode = 'no-cors';
       }
+
+      event.respondWith(
+
+        //Try to serve external content from the web, if it fails, try the cache
+        fetch(requestUrl, requestHeader).then(function(response) {
+          //if successful fetch, save a copy in the cache
+          return caches.open(fontsCacheName).then(function(cache) {
+              let clonedResponse = response.clone();
+              cache.put(requestUrl, clonedResponse);                
+              return response;
+            });
+        }).catch(function(response) {
+          console.log('error fetching from server.', response);
+          return caches.match(requestUrl).then(function(response) {
+            console.log('fetching from cache')
+            return response;
+          });           
+        })
+      );
     }
   });
   
