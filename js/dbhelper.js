62b6239b44fe8d76de81a73dd6db253925045a87
Olga Vorozheykina <solariskind@gmail.com>
2018-07-17 23:08:10 +0300

    fix: Fixed images not loaded

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -165,7 +165,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return `/img/${restaurant.photograph}.jpg`;
+    return `/img/${restaurant.id}.jpg`;
   }
   /**
    * Map marker for a restaurant.
--- js/sw.js
+++ js/sw.js
@@ -1,45 +1,46 @@
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   var urlsToCache = [
-    '../',
-    '../css/styles.css',
-    '../js/main.js',
-    '../js/restaurant_info.js',
-    '../img/1.jpg',
-    '../img/2.jpg',
-    '../img/3.jpg',
-    '../img/4.jpg',
-    '../img/5.jpg',
-    '../img/6.jpg',
-    '../img/7.jpg',
-    '../img/8.jpg',
-    '../img/9.jpg',
-    '../img/10.jpg'
+    "../",
+    "../css/styles.css",
+    "../js/main.js",
+    "../js/restaurant_info.js",
+    "../img/1.jpg",
+    "../img/2.jpg",
+    "../img/3.jpg",
+    "../img/4.jpg",
+    "../img/5.jpg",
+    "../img/6.jpg",
+    "../img/7.jpg",
+    "../img/8.jpg",
+    "../img/9.jpg",
+    "../img/10.jpg"
   ];
   event.waitUntil(
-    caches.open('restaurant-review-v1').then(function(cache) {
+    caches.open("restaurant-review-v1").then(function(cache) {
       cache.addAll(urlsToCache);
-       })
-     );
+      console.log("Cache is opened");
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   // The following will respond with cache is there is one. If not, will fetch from network
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
-        console.log('Found ', event.request.url, ' in cache');
+        console.log("Found ", event.request.url, " in cache");
         return response;
       }
       return fetch(event.request);
-      console.log('Cache matched');
-      }))
- });
+      console.log("Cache matched");
+    })
+  );
+});
 
- /**
+/**
  * Removing old caches
  */
- self.addEventListener('activate', function(event) {
-  event.waitUntil(
-  caches.delete('restaurant-review-v1')
-   );
- });
+// self.addEventListener("activate", function(event) {
+//   event.waitUntil(caches.delete("restaurant-review-v1"));
+//   console.log("Old cache cleared");
+// });
