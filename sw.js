0773c854dfd157fac87b6592c80d60c6bfcef091
Olga Vorozheykina <solariskind@gmail.com>
2018-07-18 22:03:09 +0300

    feat: Dynamic cache deletion

--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
 self.addEventListener("install", function(event) {
+  const staticCacheName = "restaurant-review-v2";
   var urlsToCache = [
     "/index.html",
     "/manifest.json",
     "/css/styles.css",
     "/js/main.js",
     "/js/restaurant_info.js",
-    "/img/1.jpg",
-    "/img/2.jpg",
-    "/img/3.jpg",
-    "/img/4.jpg",
-    "/img/5.jpg",
-    "/img/6.jpg",
-    "/img/7.jpg",
-    "/img/8.jpg",
-    "/img/9.jpg",
-    "/img/10.jpg",
+    "/img/1_1x.jpg",
+    "/img/2_1x.jpg",
+    "/img/3_1x.jpg",
+    "/img/4_1x.jpg",
+    "/img/5_1x.jpg",
+    "/img/6_1x.jpg",
+    "/img/7_1x.jpg",
+    "/img/8_1x.jpg",
+    "/img/9_1x.jpg",
+    "/img/10_1x.jpg",
+    "/img/1_2x.jpg",
+    "/img/2_2x.jpg",
+    "/img/3_2x.jpg",
+    "/img/4_2x.jpg",
+    "/img/5_2x.jpg",
+    "/img/6_2x.jpg",
+    "/img/7_2x.jpg",
+    "/img/8_2x.jpg",
+    "/img/9_2x.jpg",
+    "/img/10_2x.jpg",
     "/img/undraw_tasting_de22.svg"
   ];
   event.waitUntil(
-    caches.open("restaurant-review-v1").then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll(urlsToCache);
       console.log("Cache is opened");
     })
@@ -43,6 +54,23 @@ self.addEventListener("fetch", function(event) {
  * Removing old caches
  */
 self.addEventListener("activate", function(event) {
-  event.waitUntil(caches.delete("restaurant-review"));
-  console.log("Old cache cleared");
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      //All in promise so we wait until completion of all these promises
+      return Promise.all(
+        cacheNames
+          .filter(function(cacheName) {
+            //Filters a list of caches we don't need anymore and deletes them
+            return (
+              cacheName.startsWith("restaurant-review-") &&
+              cacheName != staticCacheName
+            );
+          })
+          .map(function(cacheName) {
+            return caches.delete(cacheName);
+            console.log("Old cache cleared");
+          })
+      );
+    })
+  );
 });
