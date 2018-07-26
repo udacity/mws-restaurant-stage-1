4bdd7624798087ffba02f8987e6450c2b3529c49
Olga Vorozheykina <solariskind@gmail.com>
2018-07-26 18:42:36 +0300

    chore: Clean up unused IDB code

--- dist/indexdb.js
+++ dist/indexdb.js
@@ -1,14 +1,19 @@
 "use strict";
 
-// import idb from "idb";
-// var idb = require("idb");
+var _idb = require("idb");
 
-var dbPromise = idb.open("restaurant-db", 1, function(upgradeDb) {
+var _idb2 = _interopRequireDefault(_idb);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// const idb = require("idb");
+
+var dbPromise = _idb2.default.open("restaurant-db", 1, function (upgradeDb) {
   var keyValStore = upgradeDb.createObjectStore("restaurants");
   console.log("DB open");
-});
+}); // "use strict";
 
-dbPromise.then(function(db) {
+dbPromise.then(function (db) {
   var tx = db.transaction("restaurants", "readwrite");
   var objectStore = tx.objectStore("restaurants");
   objectStore.put("hello", "world");
--- sw.js
+++ sw.js
@@ -42,7 +42,6 @@ self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
-        console.log("Found ", event.request.url, " in cache");
         return response;
       }
       return fetch(event.request);
