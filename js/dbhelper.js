28a3490781954128f1e20d6f723ff36e9907bd6f
Joseph <amosjoseph6@gmail.com>
2018-08-03 13:59:04 +0100

    Update file

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `/data/restaurants.json`;
   }
 
   /**
