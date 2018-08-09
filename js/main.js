b91b513bdfef587f5ea5ff538e579bcb3cbda755
scrabill <shannon.crabill@gmail.com>
2018-08-08 22:01:56 -0400

    Adds aria-roles, detailed alt text and updated headlines

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details of the restaurant ' + restaurant.name ;
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
