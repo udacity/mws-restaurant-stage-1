fb37e1db2841ca047fb52fc4319a55cd8a920e8c
chinedum onyema <onyemachinedum@gmail.com>
2018-08-09 14:58:02 +0100

    feat: Add mapbox API key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoib3dsa2luZyIsImEiOiJjamttbTBhcGMwYzF3M290ZGhyc2F4ZW1lIn0.Aasfyld5HSVNGPS-8QaDmg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
