e78452efd9ad80767aa0c3fc17b49e53f056ae18
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-08-14 21:06:40 +0100

    - improve basic accessibility

--- js/main.js
+++ js/main.js
@@ -177,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `image for ${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -90,6 +90,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `image for ${restaurant.name}`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
