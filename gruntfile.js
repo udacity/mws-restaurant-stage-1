a4208879d40b2d562ae80677ed09ce3d2d72cf3b
chinedum onyema <onyemachinedum@gmail.com>
2018-08-17 10:47:45 +0100

    chore: Configure tasks for image compression

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,71 @@
+module.exports = function(grunt) {
 
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            name: 'small',
+            width: 300,
+            quality: 30
+          },
+          {
+            name: 'normal',
+            width: 400,
+            suffix: '_1x',
+            quality: 30
+          },
+          {
+            name: 'large',
+            width: 800,
+            suffix: '_2x',
+            quality: 30
+          }]
+        },
+
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /*
+    watch: {
+      dev: {
+        files: ['src/images/*.{jpg,png}'],
+        tasks: ['responsive_images']
+      }
+    },
+    */
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    }
+
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  /*grunt.loadNpmTasks('grunt-contrib-copy');*/
+  grunt.loadNpmTasks('grunt-mkdir');
+
+  /* register default tasks */
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+
+};
