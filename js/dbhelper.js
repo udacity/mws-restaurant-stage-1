6e23b538635f8def0998e2be9339f2ad56f90c22
scrabill <shannon.crabill@gmail.com>
2018-08-21 00:18:41 -0400

    updates database url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,4 +1,4 @@
-/**
+pw/**
  * Common database helper functions.
  */
 class DBHelper {
@@ -8,8 +8,8 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337 // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
