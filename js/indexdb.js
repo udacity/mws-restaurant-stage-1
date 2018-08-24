59c67b2f0b982402b7d902fdbe6ce9e954b84029
scrabill <shannon.crabill@gmail.com>
2018-08-23 22:40:58 -0400

    Updates to indexdb

--- js/indexdb.js
+++ js/indexdb.js
@@ -1,3 +1,76 @@
 import idb from 'idb';
 
-await idb.open(…);
+await idb.open(...);
+
+const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
+  upgradeDB.createObjectStore('keyval');
+});
+
+const idbKeyval = {
+  get(key) {
+    return dbPromise.then(db => {
+      return db.transaction('keyval')
+        .objectStore('keyval').get(key);
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('keyval', 'readwrite');
+      tx.objectStore('keyval').put(val, key);
+      return tx.complete;
+    });
+  },
+  delete(key) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('keyval', 'readwrite');
+      tx.objectStore('keyval').delete(key);
+      return tx.complete;
+    });
+  },
+  clear() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('keyval', 'readwrite');
+      tx.objectStore('keyval').clear();
+      return tx.complete;
+    });
+  },
+  keys() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('keyval');
+      const keys = [];
+      const store = tx.objectStore('keyval');
+
+      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+      // openKeyCursor isn't supported by Safari, so we fall back
+      (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
+        if (!cursor) return;
+        keys.push(cursor.key);
+        cursor.continue();
+      });
+
+      return tx.complete.then(() => keys);
+    });
+  }
+};
+
+// Add things to the database
+
+keyValStore.set('foo', {hello: 'world'});
+
+// logs: {hello: 'world'}
+keyValStore.get('foo').then(val => console.log(val));
+
+// Upgrades exsiting DB
+
+const dbPromise = idb.open('keyval-store', 2, upgradeDB => {
+  // Note: we don't use 'break' in this switch statement,
+  // the fall-through behaviour is what we want.
+  switch (upgradeDB.oldVersion) {
+    case 0:
+      upgradeDB.createObjectStore('keyval');
+    case 1:
+      upgradeDB.createObjectStore('objs', {keyPath: 'id'});
+  }
+});
+
+// Reference: https://github.com/jakearchibald/idb
