7aa0f2f076bf8c6bbf9a31747bcffc28a568b3da
scrabill <shannon.crabill@gmail.com>
2018-08-25 13:34:31 -0400

    Sets up indexDB

--- js/indexdb.js
+++ js/indexdb.js
@@ -1,80 +1,14 @@
-const dbPromise = idb.open('keyval-store', 1, upgradeDB => {
-  upgradeDB.createObjectStore('keyval');
-});
+(function() {
+  'use strict';
 
-const idbKeyval = {
-  get(key) {
-    return dbPromise.then(db => {
-      return db.transaction('keyval')
-        .objectStore('keyval').get(key);
-    });
-  },
-  set(key, val) {
-    return dbPromise.then(db => {
-      const tx = db.transaction('keyval', 'readwrite');
-      tx.objectStore('keyval').put(val, key);
-      return tx.complete;
-    });
-  },
-  delete(key) {
-    return dbPromise.then(db => {
-      const tx = db.transaction('keyval', 'readwrite');
-      tx.objectStore('keyval').delete(key);
-      return tx.complete;
-    });
-  },
-  clear() {
-    return dbPromise.then(db => {
-      const tx = db.transaction('keyval', 'readwrite');
-      tx.objectStore('keyval').clear();
-      return tx.complete;
-    });
-  },
-  keys() {
-    return dbPromise.then(db => {
-      const tx = db.transaction('keyval');
-      const keys = [];
-      const store = tx.objectStore('keyval');
-
-      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
-      // openKeyCursor isn't supported by Safari, so we fall back
-      (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
-        if (!cursor) return;
-        keys.push(cursor.key);
-        cursor.continue();
-      });
-
-      return tx.complete.then(() => keys);
-    });
+  // Check for browser support
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
   }
-};
 
-// Add things to the database
+  var dbPromise = idb.open('mws-restaurant-reviews', 1);
 
-keyValStore.set('foo', {hello: 'world'});
+})();
 
-// logs: {hello: 'world'}
-keyValStore.get('foo').then(val => console.log(val));
-
-// Upgrades exsiting DB
-
-const dbPromise = idb.open('keyval-store', 2, upgradeDB => {
-  // Note: we don't use 'break' in this switch statement,
-  // the fall-through behaviour is what we want.
-  switch (upgradeDB.oldVersion) {
-    case 0:
-      upgradeDB.createObjectStore('keyval');
-    case 1:
-      upgradeDB.createObjectStore('objs', {keyPath: 'id'});
-  }
-});
-
-/* TODO
-Capture review
-Capture images
-Capture restaurant info
-Save it in indexDB
-When offline, serve cached "indexDB' version
-*/
-
-// Reference: https://github.com/jakearchibald/idb
+// Reference: https://developers.google.com/web/ilt/pwa/working-with-indexeddb
