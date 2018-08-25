3cf0ea161cd9e083586c65af8420b4113cd90e64
scrabill <shannon.crabill@gmail.com>
2018-08-25 13:58:04 -0400

    Adds function to add JSON to indexdb

--- js/indexdb.js
+++ js/indexdb.js
@@ -10,17 +10,32 @@
 
   // Creates the database
 
-  var dbPromise = idb.open('mws-restaurant-reviews', 3, function(upgradeDb) {
+  var dbPromise = idb.open('mws-restaurant-reviews', 4, function(upgradeDb) {
     console.log('Creating the database');
 
-  // If there is not an objectstore named 'restaurants', create one
+  // If there is not an objectstore named 'restaurants', create one, with a primary key of 'id'
+
     if (!upgradeDb.objectStoreNames.contains('restaurants', {keypath: 'id'} )) { 
-      upgradeDb.createObjectStore('restaurants');
+      var restaurantsOS = upgradeDb.createObjectStore('restaurants');
+
+      restaurantsOS.createIndex('neighborhood', 'neighborhood', {unique: false}) // Create index for neighborhoods
+      restaurantsOS.createIndex('cuisine', 'cuisine', {unique: false}) // Create index for cuisines
 
       console.log('Creating the restaurants objectstore');
     }
   });
 
 })();
+
+// Function for adding JSON data to indexDB
+
+dbPromise(function(db) {
+  var tx = dbPromise.transction('restaurants', 'readwrite');
+  var store = tx.objectstore('restaurants');
+
+  restaurants.add(DATABASE_URL); // ???
+
+});
+
 
 // Reference: https://developers.google.com/web/ilt/pwa/working-with-indexeddb
