2b2a29a8e82387b6c48581daf2e3a5d69471bb47
scrabill <shannon.crabill@gmail.com>
2018-08-25 14:17:44 -0400

    Moves indedb set up to dbhelper

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,4 +1,38 @@
 /**
+* Set up IndexDB
+*/
+
+(function() {
+  'use strict';
+
+  // Check for browser support
+
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
+
+  // Creates the database
+
+  var dbPromise = idb.open('mws-restaurant-reviews', 5, function(upgradeDb) {
+    console.log('Creating the database');
+
+  // If there is not an objectstore named 'restaurants', create one, with a primary key of 'id'
+
+    if (!upgradeDb.objectStoreNames.contains('restaurants', {keypath: 'id'} )) { 
+      var restaurantsOS = upgradeDb.createObjectStore('restaurants');
+
+      restaurantsOS.createIndex('neighborhood', 'neighborhood', {unique: false}) // Create index for neighborhoods
+      restaurantsOS.createIndex('cuisine', 'cuisine', {unique: false}) // Create index for cuisines
+
+      console.log('Creating the restaurants objectstore');
+    }
+  });
+
+})();
+
+
+/**
  * Common database helper functions.
  */
 class DBHelper {
