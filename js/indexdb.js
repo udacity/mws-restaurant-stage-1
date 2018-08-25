c68e972be6aa4319fe5822b5f1507e78e85fa9d7
scrabill <shannon.crabill@gmail.com>
2018-08-25 13:41:13 -0400

    Creates object store for restaurants

--- js/indexdb.js
+++ js/indexdb.js
@@ -2,12 +2,21 @@
   'use strict';
 
   // Check for browser support
+
   if (!('indexedDB' in window)) {
     console.log('This browser doesn\'t support IndexedDB');
     return;
   }
 
-  var dbPromise = idb.open('mws-restaurant-reviews', 1);
+  // Creates the database
+
+  var dbPromise = idb.open('mws-restaurant-reviews', 2, function(upgradeDb) {
+    console.log('Creating the database');
+
+    if (!upgradeDb.objectStoreNames.contains('restaurants')) { // If there is not an objectstore named 'restaurants', create one
+      upgradeDb.createObjectStore('restaurants');
+    }
+  });
 
 })();
 
