8497949f60cd4e279fb89f2ff0e3d630f8e85788
Traci <traci.kamp@blackburn.edu>
2018-08-26 12:58:36 -0500

    Add a function to determine the day of the week

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -176,3 +176,24 @@ getParameterByName = (name, url) => {
     return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+const getDayOfWeek = (day) => {
+  switch(day) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+      return undefined;
+  }
+}
