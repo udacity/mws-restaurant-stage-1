aa9ad7b512f5ed721110df04b1b7f28f292e9d0f
dotPing <31798381+PingSharp@users.noreply.github.com>
2018-08-28 11:52:34 +0200

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,13 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    if(window.location.hostname === "localhost" || location.hostname === "127.0.0.1" || window.location.hostname.indexOf(myUrlPattern) >= 0){
+      return `http://localhost:${port}/data/restaurants.json`;
+    }
+    else{
+      return "https://pingsharp.github.io/mws-restaurant-stage-1/data/restaurants.json";
+    }
+    
   }
 
   /**
