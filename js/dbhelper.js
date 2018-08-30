83856bd8b18b91d73537e91492df38ca6815a8e9
Auwal MS <auwalforall@gmail.com>
2018-08-30 13:30:08 +0100

    update:  changed the restaurant to fetch info from the server.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -11,27 +11,22 @@ class DBHelper {
         return `http://localhost:${port}/restaurants`;
     }
 
+    static getRestaurantFromServer() {
+        return fetch(DBHelper.DATABASE_URL)
+            .then(response => {
+                return response.json();
+            })
+            .then(restaurants => {
+                console.log(restaurants);
+                return restaurants;
+            });
+    }
+
     /**
      * Fetch all restaurants.
      */
     static fetchRestaurants(callback) {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", DBHelper.DATABASE_URL);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                // Got a success response from server!
-                const json = JSON.parse(xhr.responseText);
-                const restaurants = json.restaurants;
-                callback(null, restaurants);
-            } else {
-                // Oops!. Got an error from server.
-                const error = `Request failed. Returned status of ${
-                    xhr.status
-                }`;
-                callback(error, null);
-            }
-        };
-        xhr.send();
+        return DBHelper.getRestaurantFromServer();
     }
 
     /**
