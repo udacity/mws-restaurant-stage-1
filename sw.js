251e6fc7d9d3e050025e097cac990ff56276e615
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-09-01 13:11:21 -0400

    fix: mis-spelled sw-registration.js

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
 let staticCacheName = 'restaurant-reviews-v1';
+let urlsToCache = [
+    '/',
+    './sw-registration.js',
+    'index.html',
+    'restaurant.html',
+    'data/restaurants.json',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/dbhelper.js',
+    'css/styles.css',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg'
+];
 
 //credit Traversy Media for inspiration https://www.youtube.com/watch?v=ksXwaWHCW6k
 //Save content in cache
 self.addEventListener('install', function(event) {
     console.log('Service Worker: Installed');
-
-    let urlsToCache = [
-        '/',
-        // '/sw-registration.js',
-        'index.html',
-        'restaurant.html',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'js/dbhelper.js',
-        'css/styles.css',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
-    ];
 
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
@@ -53,18 +53,11 @@ self.addEventListener('activate', function(event) {
 
 //Show offline via fetch
 self.addEventListener('fetch', function(event) {
-    //if event.response === offline {}
     console.log('Service Worker: Fetching');
-    //Traversy code
     event.respondWith(
         //if site is offline, then event.request will fail and go to .catch
-        fetch(event.request).catch(function(){
-            console.log('Service Worker: You are offline');
-            staticCacheName.match(event.request);
-        })
+        fetch(event.request).catch(() =>
+            caches.match(event.request)
+       )
     );
-    //mycode
-    // caches.open(staticCacheName).then(function(cache) {
-    //     cache.match('request?');
-    // });
 });
