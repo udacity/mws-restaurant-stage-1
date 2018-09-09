5b584cc2c1629a5aa9b7d0730e1f60289499b96d
Michael Robards <mikerobards@icloud.com>
2018-09-09 08:36:27 -0400

    add aria to restaurant.html

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -127,7 +127,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
