7226596628b25800de54cd12073eb36e0a3e0753
Brian Freeman <atlanta_dancer@hotmail.com>
2018-09-10 10:51:57 -0400

    improve caching and offline access for details

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var staticCacheName='mws-restaurant-v1';
+var staticCacheName='mws-restaurant-v6';
 // Service worker install built upon code used for Wittr project
 self.addEventListener('install', function(event) {
   console.log('install');
@@ -8,9 +8,9 @@ self.addEventListener('install', function(event) {
       return cache.addAll([
         '/index.html',
         '/restaurant.html',
-        'data/restaurants.json',
-        'js/main.js',
         'js/dbhelper.js',
+        'js/idb.js',
+        'js/main.js',
         'js/register.js',
         'js/restaurant_info.js',
         'css/styles.css'
@@ -18,17 +18,72 @@ self.addEventListener('install', function(event) {
     })
   );
 });
-//Fetch code build upon code from Wittr project
+//Fetch code build upon code from my Wittr project
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
 // for now do simple cache of all requests 
-console.log('in fetch for ',requestUrl);
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      console.log('in match');  //,response);
-      return response || fetch(event.request);
+console.log('in fetch for ',requestUrl,'path',requestUrl.pathname);
+
+if (requestUrl.origin === location.origin) {
+  // for unspecified offline access redirect to cached index.html
+  if (requestUrl.pathname === '/') {
+    event.respondWith(caches.match('/index.html'));
+    return;
+  }
+  // return general images from cache or network
+  if (requestUrl.pathname.startsWith('/img/')) {
+    event.respondWith(servePhoto(event.request));
+    return;
+  }
+
+  // restaurant detail pages 
+  if (requestUrl.pathname.startsWith('/restaurant.html')) {
+    event.respondWith(serveDetails(event.request));
+    return;
+  }
+}
+//any other pages check cache & fetch if needed
+event.respondWith(
+  caches.open(staticCacheName).then(function(cache) {
+    return cache.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    });
+
     }).catch(function(e) {
       console.log('fetch promise failed for ',e);
     })
   );
 });
+
+
+// copied/adapted from my wittr project code
+function servePhoto(request) {
+  // console.log('main img url',request.url);
+  var storageUrl = request.url.replace(/-\-sm|-med|-lg\.jpg$/, '');
+  // console.log('storage url',storageUrl);
+  return caches.open(staticCacheName).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
+
+// copied/adapted from my wittr project code
+function serveDetails(request) {
+  // console.log('main detail url',request.url);
+  return caches.open(staticCacheName).then(function(cache) {
+    return cache.match(request.url).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(request.url, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
