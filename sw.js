65d7e0a75176bc96695443f06643d2c0b81744ae
chinedum onyema <onyemachinedum@gmail.com>
2018-09-11 10:22:41 +0100

    fix: A minor bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'Restaurant-static-v3'; 
+const staticCacheName = 'Restaurant-static-v2'; 
 const filesToCache = [
     './',
     './index.html',
@@ -71,7 +71,7 @@ self.addEventListener('fetch', event => {
     }
 /** fetch from catch or network **/
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(requestUrl).then(response => {
       return response || fetch(event.request).then(fetchResponse => { 
         return caches.open(staticCacheName).then(cache => {           
           cache.put(requestUrl, fetchResponse.clone());            
