326d5d7480e46773291fa539596e806d404a9ee8
oreanroy <oreanroy@gmail.com>
2018-10-05 22:19:21 +0530

    improved assesibility as per code review

--- js/main.js
+++ js/main.js
@@ -150,7 +150,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", `${restaurant.name} restaurant image`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
