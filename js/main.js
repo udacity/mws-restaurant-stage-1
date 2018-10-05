1519ef2945010cc12d732f4c53d18e2ebf14b197
oreanroy <oreanroy@gmail.com>
2018-10-05 16:08:17 +0530

    Responsive css added

--- js/main.js
+++ js/main.js
@@ -157,6 +157,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.classList.add("card");
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
