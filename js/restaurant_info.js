0fb5e6a33e76a15723e7e28e015c45e60fd1de87
Raveesh Agarwal <raveeshr503@gmail.com>
2018-11-12 14:51:37 +0530

    adds api keys to main and restaurant_info

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoicmF2ZWVzaCIsImEiOiJjam1nMjEzeW42dDZzM2tsaTZxZnh5YnI4In0.PFrlTLrSURsL2xj7rpN4rw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
