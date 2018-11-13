11bfcbd563d62f3c3ce5c685f150e3619d3dd47f
Alexis Koss <alexis30@uw.edu>
2018-11-13 02:39:10 -0800

    style: Update styles for cards to be responsive

--- js/main.js
+++ js/main.js
@@ -163,22 +163,29 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const text = document.createElement('div');
+  text.className = "restaurant-text";
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  text.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  neighborhood.className = "restaurant-neighborhood";
+  text.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  address.className = "restaurant-address";
+  text.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  const button = document.createElement('button');
+  button.innerHTML = 'View Details';
+  button.href = DBHelper.urlForRestaurant(restaurant);
+  text.append(button)
+
+  li.append(text);
 
   return li
 }
