887a3394ff3d124b14ddcaad865cba74b7ec5337
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-12-20 12:59:59 +0300

    Updated Database url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,8 +7,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `https://bl4ck4ndbr0wn.github.io/mws-restaurant-stage-1/data/restaurants.json`;
   }
 
   /**
