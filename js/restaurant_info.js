c6af1ea04f06b6e5a1808e629903a978ed1532d3
Shavaun Brandy <shavaunbrandy@msn.com>
2019-02-04 02:08:06 -0500

    Added References

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2hvbm5pZTAyIiwiYSI6ImNqcmdxNGZsMTAzeHA0YW5ucDBuYWR5aDkifQ.ePJKii7JlFVa2BItuZDu2Q',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -88,8 +88,9 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = `Picture of ${restaurant.name}`; //Concept utilized from "A Walkthrough by Alexandro Perez"
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
