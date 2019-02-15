e4f79d400f0f772ff563dc94c87128628490d446
sunny chouhan <snychn01@gmail.com>
2019-02-15 21:21:34 +0530

    add event listener fetch

--- serviceworker.js
+++ serviceworker.js
@@ -46,3 +46,14 @@ self.addEventListener('activate', function(event) {
 })
 
 
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
+
+
